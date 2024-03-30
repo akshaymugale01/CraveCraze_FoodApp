f5ac2cdbee085fedb55e7bc8a1ad09fb5361b5c4
akshaymugale01 <139070784+akshaymugale01@users.noreply.github.com>
2024-03-31 02:03:37 +0530

    Fisrt Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
+
+import Home from './screens/Home';
+import Login from "./screens/Login";
+
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap'
+import 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js';
+import Signup from "./screens/Signup";
+import { ToastContainer } from "react-toastify";
+import { CartProvider } from "./components/ContextReducer";
+import Cart from "./screens/Cart";
+import MyOrder from "./screens/MyOrder";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <CartProvider>
+    <Router>
+    <div>
+      <ToastContainer class="custom-toast-container"/>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/createuser" element={<Signup />} />
+        <Route exact path="/myOrder" element={<MyOrder />} />
+      </Routes>
     </div>
+  </Router>
+  </CartProvider>
   );
 }
 
